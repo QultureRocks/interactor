9d67881574c51552fc604a1eec3811b71dd05e65
GabrielaGuedes <gabriela.guedes.gmg@gmail.com>
2020-08-24 15:52:47 -0300

    add condition to organizer with specs

--- spec/interactor/organizer_spec.rb
+++ spec/interactor/organizer_spec.rb
@@ -7,13 +7,14 @@ module Interactor
     describe ".organize" do
       let(:interactor2) { double(:interactor2) }
       let(:interactor3) { double(:interactor3) }
+      let(:interactor4) { double(:interactor4) }
 
       it "sets interactors given class arguments" do
         expect {
           organizer.organize(interactor2, interactor3)
         }.to change {
           organizer.organized
-        }.from([]).to([interactor2, interactor3])
+        }.from([]).to([{ interactor: interactor2, condition: true }, { interactor: interactor3, condition: true }])
       end
 
       it "sets interactors given an array of classes" do
@@ -21,7 +22,15 @@ module Interactor
           organizer.organize([interactor2, interactor3])
         }.to change {
           organizer.organized
-        }.from([]).to([interactor2, interactor3])
+        }.from([]).to([{ interactor: interactor2, condition: true }, { interactor: interactor3, condition: true }])
+      end
+
+      it "sets interactors given class arguments mixed with hash argument" do
+        expect {
+          organizer.organize(interactor2, { interactor: interactor3, condition: false }, interactor4)
+        }.to change {
+          organizer.organized
+        }.from([]).to([{ interactor: interactor2, condition: true }, { interactor: interactor3, condition: false }, { interactor: interactor4, condition: true }])
       end
     end
 
@@ -37,11 +46,17 @@ module Interactor
       let(:interactor2) { double(:interactor2) }
       let(:interactor3) { double(:interactor3) }
       let(:interactor4) { double(:interactor4) }
+      let(:interactor5) { double(:interactor5) }
 
       before do
         allow(instance).to receive(:context) { context }
         allow(organizer).to receive(:organized) {
-          [interactor2, interactor3, interactor4]
+          [
+            { interactor: interactor2, condition: true }, 
+            { interactor: interactor3, condition: true }, 
+            { interactor: interactor4, condition: true }, 
+            { interactor: interactor5, condition: false }
+          ]
         }
       end
 
@@ -49,6 +64,7 @@ module Interactor
         expect(interactor2).to receive(:call!).once.with(context).ordered
         expect(interactor3).to receive(:call!).once.with(context).ordered
         expect(interactor4).to receive(:call!).once.with(context).ordered
+        expect(interactor4).not_to receive(:call!)
 
         instance.call
       end
